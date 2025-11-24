aa911b4a2124b16456d7f85df6cddb7c71a9844f
omarrui <rouiguio919@gmail.com>
2025-11-24 18:50:40 +0100

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,4 +2,14 @@
 """function that returns the sum of a + b"""
 
 def add (a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+
+    Args:
+        a (float): The first float.
+        b (float): The second float.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
