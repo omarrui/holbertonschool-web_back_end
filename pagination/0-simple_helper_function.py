d84cf6886b094ab85cd286af8d217415237c9d99
omarrui <rouiguio919@gmail.com>
2025-12-01 18:11:34 +0100

    fixing the task

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,5 @@ def index_range(page, page_size):
     """a functiont that takes two variables for the index range"""
     index_range = (page, page_size)
     start = (page - 1) * page_size
-    end = page - page_size
+    end = start + page_size
     return (start, end)
