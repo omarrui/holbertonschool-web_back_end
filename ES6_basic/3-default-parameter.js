acb5aec5ecdf92fdd430a3b34c457b1bb80ccff3
omarrui <rouiguio919@gmail.com>
2025-12-10 14:34:12 +0100

    task 3

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(a, b = 10, c = 20) {
-    return a + b + c;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+    return initialNumber + expansion1989 + expansion2019;
 }
