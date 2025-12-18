b7674dc35ee904f9cd1eb1eb54b289d1e6088240
omarrui <rouiguio919@gmail.com>
2025-12-18 09:41:57 +0100

    task 1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-    return new promise ((resolve, reject) =>{
-        if (success){
-            resolve({
-                status: 200,
-                body: 'sucess',
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
