39aabea6da3f822b33d5589228d4badac040140a
omarrui <rouiguio919@gmail.com>
2026-01-09 20:01:50 +0100

    task 7

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-  if (name) process.stdout.write(`Your name is: ${name}`);
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (data) => {
+    const name = data.trim();
+
+    process.stdout.write(`Your name is: ${name}\n`)
+
+    process.exit();
 });
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+
+process.on ('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+})
