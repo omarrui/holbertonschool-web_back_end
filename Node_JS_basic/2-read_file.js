e658b30d093f16c6829a180c0751d36996eb0d70
omarrui <rouiguio919@gmail.com>
2026-01-09 21:20:35 +0100

    task 3

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fieldGroups = {};
+
+    students.forEach((student) => {
+      const fields = student.split(',');
+      const firstName = fields[0];
+      const field = fields[3];
+
+      if (!fieldGroups[field]) {
+        fieldGroups[field] = [];
+      }
+      fieldGroups[field].push(firstName);
+    });
+    for (const field in fieldGroups) {
+      if (Object.prototype.hasOwnProperty.call(fieldGroups, field)) {
+        const studentsList = fieldGroups[field].join(', ');
+        console.log(`Number of students in ${field}: ${fieldGroups[field].length}. List: ${studentsList}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
